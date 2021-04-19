a0eaf9ebbe9a31c16050c4953d7e0e23b09ff761
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2021-04-19 19:35:01 +0200

    interface context added

--- src/contexts/ExpenseAppContext.tsx
+++ src/contexts/ExpenseAppContext.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import { createContext } from 'react';
 import { ExpenseItems } from '../services/expensesService/useExpenseService'
+
+interface ExpenseContextValues {
+  expenseResults: ExpenseItems[],
+  setExpensesResults: React.Dispatch<React.SetStateAction<ExpenseItems[]>>,
+  useExpenseService: () => { expensesAPI: ExpenseItems[] }
+}
+
 export const ExpenseAppContext = 
-  createContext<{ expenseResults: ExpenseItems[], setExpensesResults: React.Dispatch<React.SetStateAction<ExpenseItems[]>>, useExpenseService: () => { expensesAPI: ExpenseItems[] } } | null>(null);
+  createContext< ExpenseContextValues | null>(null);
 
