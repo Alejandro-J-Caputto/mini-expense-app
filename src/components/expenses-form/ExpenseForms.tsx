a0be2c248424849fc0d76c2754919be8eef9e271
Alejandro Caputto frias <alejandro_caputto@MacBook-Air-de-Alejandro.local>
2021-04-21 09:16:06 +0200

    toggle Menu Expenses onClick

--- src/components/expenses-form/ExpenseForms.tsx
+++ src/components/expenses-form/ExpenseForms.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ExpenseModel } from '../../classes/expenseModel';
 import { useForm, Validatable } from '../../hooks/useForm';
 export interface ExpenseForm {
@@ -16,6 +17,10 @@ export const ExpenseForms = ({ onSubmittedExpense }: { onSubmittedExpense: (expe
   const { formValues, handleInputChange, reset, customValidator } = useForm<ExpenseForm>({ expense: '', date: new Date(), price: 0.1 })
   const { expense, date, price } = formValues;
   
+  const [hideForm, setHideForm] = useState(true)
+
+  const buttonFormHandler =<div className="expense-form__action expense-form__action--menu"><button onClick={()=> setHideForm(false)} className="btn btn--expense-form btn--expense--menu"> Add Expense </button></div> 
+
   const expenseIsValid: Validatable = {
     value: expense,
     required: true,
@@ -38,49 +43,56 @@ export const ExpenseForms = ({ onSubmittedExpense }: { onSubmittedExpense: (expe
 
     const newExpense = new ExpenseModel(new Date(date), expense, price)
     onSubmittedExpense(newExpense);
+
     reset()
   }
 
+
   return (
+
     <form onSubmit={submitHandler} className="expense-form">
+      {hideForm ? buttonFormHandler :
       <div className="expense-form__controls">
-        <div className="expense-form__control">
-          <label htmlFor="expense">Title</label>
-          <input
-            name='expense'
-            value={expense}
-            onChange={handleInputChange}
-            placeholder="Expense"
-            id="expense"
-            type="text" />
-        </div>
-        <div className="expense-form__control">
-          <label htmlFor="price">Amount</label>
-          <input
-            id="price"
-            value={price}
-            name="price"
-            onChange={handleInputChange}
-            placeholder="Amount"
-            type="number"
-            min="0.01"
-            step="0.01" />
-        </div>
-        <div className="expense-form__control">
-          <label htmlFor="date">Date</label>
-          <input
-            id="date"
-            value={date.toString()}
-            name="date"
-            onChange={handleInputChange}
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31" />
-        </div>
-        <div className="expense-form__action">
-          <button className="btn btn--expense-form" type="submit"> Add Expense </button>
-        </div>
+      <div className="expense-form__control">
+        <label htmlFor="expense">Title</label>
+        <input
+          name='expense'
+          value={expense}
+          onChange={handleInputChange}
+          placeholder="Expense"
+          id="expense"
+          type="text" />
       </div>
+      <div className="expense-form__control">
+        <label htmlFor="price">Amount</label>
+        <input
+          id="price"
+          value={price}
+          name="price"
+          onChange={handleInputChange}
+          placeholder="Amount"
+          type="number"
+          min="0.01"
+          step="0.01" />
+      </div>
+      <div className="expense-form__control">
+        <label htmlFor="date">Date</label>
+        <input
+          id="date"
+          value={date.toString()}
+          name="date"
+          onChange={handleInputChange}
+          type="date"
+          min="2019-01-01"
+          max="2022-12-31" />
+      </div>
+      <div className="expense-form__action">
+        <button className="btn btn--expense-form" type="submit"> Add </button>
+        <button onClick={()=>setHideForm(true)} className="btn btn--expense-form btn--expense-form--cancel"> Cancel </button>
+      </div>
+    </div>
+      }
+      
     </form>
   )
 }
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,6 +5,7 @@ import { ExpensesItem } from '../../components/expenses-item/ExpensesItem'
 import { ExpensesCard } from '../../components/expensesCard/ExpensesCard'
 import { NewExpenses } from '../../components/newExpense/NewExpenses'
 import { ExpenseFilter } from '../../components/expense-filter/ExpenseFilter'
+import { ExpenseItems } from '../../services/expensesService/useExpenseService'
 
 
 export const MainPage = () => {
@@ -18,32 +19,35 @@ export const MainPage = () => {
   }, [expenseResults])
 
   const filteredExpenseResults = expenseResults.filter(exp => new Date(exp.date).getFullYear() === +filterYear)
-  
+
 
   const onSetYear = (year: string) => {
     setFilterYear(year);
   }
 
+  let expenseListContent: any = 
+    <div className="message-filter">
+      <p className="message-filter__notFound"> No expenses registered </p>
+    </div> as JSX.Element
+
+  if(filteredExpenseResults.length > 0 ) {
+    expenseListContent = filteredExpenseResults.map(({ id, expense, price, date }) => {
+      return <ExpensesItem
+        dispatchExpense={dispatchExpense}
+        key={id}
+        expense={expense}
+        price={price}
+        date={date}
+      ></ExpensesItem> 
+    })
+  }
 
   return (
     <div className="main">
       <ExpensesCard className="container">
         <ExpenseFilter filterYear={filterYear} onSetYear={onSetYear}></ExpenseFilter>
         <NewExpenses></NewExpenses>
-        {!filteredExpenseResults.length ?
-        <div className="message-filter">
-          <p className="message-filter__notFound"> No expenses registered </p> 
-        </div> 
-        :filteredExpenseResults.map(({ id, expense, price, date }) => {
-          return <ExpensesItem
-            dispatchExpense={dispatchExpense}
-            key={id}
-            expense={expense}
-            price={price}
-            date={date}
-          ></ExpensesItem>
-        })
-        }
+        {expenseListContent}
       </ExpensesCard>
     </div>
   )
